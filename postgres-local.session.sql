00a72c520a8cce52efbf09b7ffd9818302052853
Abduullahh <abduuullxhhh@gmail.com>
2023-02-26 12:22:52 +0200

    trying some data manipulation

--- postgres-local.session.sql
+++ postgres-local.session.sql
@@ -1,5 +1,14 @@
-SELECT COUNT(*) FROM test;
-SELECT COUNT (DISTINCT ride_id) AS total, member_casual FROM test
+SELECT COUNT( DISTINCT ride_id) FROM test;
+SELECT COUNT (ride_id) AS total, ((CAST(COUNT(ride_id) AS NUMERIC)/5754248) * 100) AS percent_total,member_casual
+FROM test
 GROUP BY member_casual
-ORDER BY total DESC; -- to know how many each of the riders and sort the result by the highest total
-SELECT * FROM test LIMIT 10;
+ORDER BY total DESC; -- to know how many each of the riders and their percentege and sort the result by the highest total
+SELECT  *
+FROM test
+LIMIT 1000;
+SELECT ride_id, started_at, ended_at, member_casual,start_station_name,
+COALESCE(end_station_name, end_station_id), (ended_at - started_at) AS ride_length
+FROM test
+WHERE member_casual = 'casual'
+ORDER BY ride_length DESC
+LIMIT 1000;

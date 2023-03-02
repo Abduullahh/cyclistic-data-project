a377b03ce2bcd7e69b460120c4b28bdb630d9b17
Abduullahh <abduuullxhhh@gmail.com>
2023-03-02 10:42:56 +0200

    Sql file
    modified the sections and ordered them

--- postgres-local.session.sql
+++ postgres-local.session.sql
@@ -1,16 +1,6 @@
-SELECT COUNT( DISTINCT ride_id) FROM test;
-SELECT COUNT (ride_id) AS total, ((CAST(COUNT(ride_id) AS NUMERIC)/5754248) * 100) AS percent_total,member_casual
-FROM test
-GROUP BY member_casual
-ORDER BY total DESC; -- to know how many each of the riders and their percentege and sort the result by the highest total
+-------------------------------------------------------------------------------------------------------
+--------------------------------------/* PROCESS STEP */-----------------------------------------------
 
-SELECT ride_id, started_at, ended_at, member_casual,start_station_name,
-COALESCE(end_station_name, end_station_id), (ended_at - started_at) AS ride_length
-FROM test
-WHERE member_casual = 'casual'
-ORDER BY ride_length DESC
-LIMIT 1000;
----------------------------------------------------------------------------------------------------------------------------
 -- to make sure that there's no duplicates in the ride_id column.
 SELECT COUNT(ride_id)
 FROM test; -- 5754248 ROWS
@@ -150,4 +140,18 @@ SELECT COLUMN_NAME, DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'public' AND
 TABLE_NAME = 'test';
+-------------------------------------------------------------------------------------------------------
+--------------------------------------/* ANALYZE STEP */-----------------------------------------------
 
+SELECT COUNT( DISTINCT ride_id) FROM test;
+SELECT COUNT (ride_id) AS total, ((CAST(COUNT(ride_id) AS NUMERIC)/5754248) * 100) AS percent_total,member_casual
+FROM test
+GROUP BY member_casual
+ORDER BY total DESC; -- to know how many each of the riders and their percentege and sort the result by the highest total
+
+SELECT ride_id, started_at, ended_at, member_casual,start_station_name,
+COALESCE(end_station_name, end_station_id), (ended_at - started_at) AS ride_length
+FROM test
+WHERE member_casual = 'casual'
+ORDER BY ride_length DESC
+LIMIT 1000;
